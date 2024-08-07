a1028336ea6f45c5fe06a529b15e4f9529957b90
Sharanya M S <sharanyamavinaguni@gmail.com>
2024-08-08 01:08:06 +0530

    Update and rename 8a to 8a.py

--- 8a.py
+++ 8a.py
@@ -4,7 +4,7 @@ def odd_numbers():
     numbers = [random.randint(10, 99) for _ in range(20)]
     print(f"Generated Numbers: {numbers}")
     odd_numbers = [num for num in numbers if num % 2 != 0 and len(str(num)) in [2, 4]]
-    print(f"Odd Numbers of Length 2 and 4: {odd_numbers}")
+    print(f"Odd Numbers of Length 2 and 4: {list(odd_numbers)}")
 
 odd_numbers()
 
